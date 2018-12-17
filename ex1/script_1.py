018f4692a6d8c2671e257e20dc76ebd8daa8a497
ananthrajsingh <ananthraj.singh@gmail.com>
2018-12-17 21:46:15 +0530

    Initial Commit

---
+++ ex1/script_1.py
@@ -0,0 +1,5 @@
+from subprocess import call
+import clear_screen
+# command = "Users/ananthrajsingh/Desktop/Andrew_Ng_Python/ex1/clear_screen.py"
+# call(command, shell = True)
+clear_screen.clear()
