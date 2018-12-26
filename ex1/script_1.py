aab95f2dc2108e973ef225d1f45d805736bb1055
ananthrajsingh <ananthraj.singh@gmail.com>
2018-12-26 17:24:04 +0530

    Ex1 Drawing Fit

--- ex1/script_1.py
+++ ex1/script_1.py
@@ -5,32 +5,26 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cost
 
-# command = "Users/ananthrajsingh/Desktop/Andrew_Ng_Python/ex1/clear_screen.py"
-# call(command, shell = True)
+
 clear_screen.clear()
-# print("Pausing the script")
-# input("Press <ENTER> to continue")
-# print("Continuing...")
 filepath1 = "/Users/ananthrajsingh/Desktop/Andrew_Ng_Python/ex1/ex1data1.csv"
 names1 = ["profit", "population"]
 filepath2 = "Users/ananthrajsingh/Desktop/Andrew_Ng_Python/ex1/ex1data2.csv"
 dataset1 = pandas.read_csv(open(filepath1), names = names1, decimal = ",")
-# print("Printing dataset")
-# input("Press <ENTER> to print dataset")
-# print(dataset1)
+
 input("Press <ENTER> to continue")
 clear_screen.clear()
 print("Plotting data...")
 array1 = dataset1.values
 X = array1[:,0]
-X = X.astype(np.float)
+X = X.astype(np.float) # Since array was made of String
 y = array1[:,1]
 y = y.astype(np.float)
-
-plt.scatter(X,y)
-# plt.axis([0, 5, 0, 5])
-# fig.suptitle("Profit vs population")
-plt.show()
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+ax1.scatter(X,y, label = "Data")
+plt.draw()
+fig.show()
 
 
 #####################################################################
@@ -43,11 +37,7 @@ X = np.matrix(X)
 X = X.transpose()
 y = np.matrix(y)
 y = y.transpose()
-# print("Shape of y")
-# print(y.shape)
 n,m = X.shape
-# print("Shape of X")
-# print(X.shape)
 # Make an column of 1s of size m
 zeros = np.ones((n,1))
 # Adding a column of 1's to X to add bias unit
@@ -77,20 +67,27 @@ print("Theta found by gradientDescent")
 print(theta)
 print("Expected theta values (approx)")
 print(" -3.6303 \n 1.1664")
+# X is m x 2 and theta is 2 x 1
+# theta_t = np.transpose(theta)
+ax1.scatter([X[:,1]], [X * theta],color = "k", marker = ".", label = "Fit")
+plt.draw()
+fig.show()
+plt.legend(loc='best')
+# plt.show()
 input("Press <ENTER> to continue")
 
 #####################################################################
 # Visualizing Data
 #####################################################################
-theta0_values = np.arange(-10, 10, 0.2)
-theta1_values = np.arange(-1, 4, 0.05)
-J_vals = np.zeros((len(theta0_values), len(theta1_values)), dtype = "float")
+# theta0_values = np.arange(-10, 10, 0.2)
+# theta1_values = np.arange(-1, 4, 0.05)
+# J_vals = np.zeros((len(theta0_values), len(theta1_values)), dtype = "float")
 
 
-for i in range(len(theta0_values)):
-	for j in range(len(theta1_values)):
-		t = [[theta0_values[i]],[theta1_values[j]]
-		J_vals[i][j] = cost.computeCost(X, y, t);
-print(J_vals)
+# for i in range(len(theta0_values)):
+# 	for j in range(len(theta1_values)):
+# 		t = [[theta0_values[i]],[theta1_values[j]]
+# 		J_vals[i][j] = cost.computeCost(X, y, t);
+# print(J_vals)
 
 
