220752482e7c9c1f0c72ad3051c1f36aba5f5205
ananthrajsingh <ananthraj.singh@gmail.com>
2018-12-28 19:30:34 +0530

    Ex2 Cost and Gradient for Logistic Regression

---
+++ ex2/regression.py
@@ -0,0 +1,20 @@
+# This will have all the helper functions for this exercise
+# Here goes the import statements
+import numpy as np 
+import math
+def sigmoid(z):
+	return 1/(1 + np.exp(z))
+
+# This will be used to calculate cost and gradient for logistic regression
+
+def costFunction(theta, X, y):
+	# First let us get number of examples we are dealing with
+	m,n = y.shape
+	a,b = theta.shape
+	# Initialize cost and gradient
+	J = 0
+	grad = np.zeros((a,b))
+	A = np.log(np.transpose(sigmoid(X * theta))) * (-y) + np.log(1 - np.transpose(sigmoid(X * theta))) * (1-y)
+	J = A/m
+	grad = (np.transpose(X) * (sigmoid(X * theta) - y))/m
+	return (J, grad)
--- ex2/script_2.py
+++ ex2/script_2.py
@@ -6,15 +6,15 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import seaborn as sns
 from numpy import where
-# import regression
-
+import regression
+screen.clear()
 # As always, first step is loading up the data
 filepath = "/Users/ananthrajsingh/Desktop/Andrew_Ng_Python/ex2/ex2data1.csv"
 # This is mndatory, else pandas will use first example as heading
 names = ["marks1", "marks2", "admission"]
 dataset = pd.read_csv(open(filepath), names = names, decimal = ",")
-print(dataset.shape)
-print(list(dataset.columns))
+# print(dataset.shape)
+# print(list(dataset.columns))
 dataset_array = dataset.values
 dataset_array = np.matrix(dataset_array)
 # Let us separate X and y from this array
@@ -26,22 +26,51 @@ y = y.astype(np.float)
 # Better to convert these to numpy matrices
 X = np.matrix(X)
 y = np.matrix(y)
+
+m,n = X.shape
 # Display spome of data
 print("First 5 examples: ")
 print(X[0:5, :])
 print(y[0:5, :])
 
+input("Program paused. Press <enter> to see data plot.")
+
+
+#####################################################################
+# VISUALIZE DATA
+#####################################################################
 
 # Now we have arrays we can work on
 # Let us visualize the data
 pos, temp = where(y == 1)
-print(pos)
 neg, temp = where(y == 0)
-print(neg)
 # np.squeeze(np.asarray(M)) is used to convert matrix to 1D array,
 # as scatter accepts 1d array.
-print("shape of X[pos, 0]: ", np.squeeze(X[pos, 0]).shape)
 plt.scatter(np.squeeze(np.asarray(X[pos, 0])), np.squeeze(np.asarray(X[pos, 1])), marker = "o", c = "r")
 plt.scatter(np.squeeze(np.asarray(X[neg, 0])), np.squeeze(np.asarray(X[neg, 1])),  marker = "x", c = "b")
+print("Close plot window to continue.")
 plt.show()
+
+
+#####################################################################
+# COMPUTE COST AND GRADIENT
+#####################################################################
+
+# Let us first add intercept term
+intercept_ones = np.ones((m,1))
+X = np.hstack((intercept_ones, X))
+
+# Initialize theta
+initial_theta = np.zeros((n+1,1))
+
+# let us calculate the cost and gradient
+cost, gradient = regression.costFunction(initial_theta, X, y)
+
+print('Cost at initial theta (zeros): \n', cost);
+print('Expected cost (approx): 0.693\n');
+print('Gradient at initial theta (zeros): \n');
+print(' \n', gradient);
+print('Expected gradients (approx):\n -0.1000\n -12.0092\n -11.2628\n');
+
+
 
